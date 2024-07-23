8c3eeac446e583d2f1c079e284c20683e0b096d2
prasad <prasadbhujbal1407@gmail.com>
2024-07-23 17:22:55 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# GenQuery
+SQL Chatbot powered by GenAI, simplifying data access and manipulation in SQL databases through natural language commands and educational examples.
+
+# SQL Bot 🤖💻
+Welcome to SQL Bot! 🚀 Your friendly assistant for simplifying SQL querying and enhancing your programming skills. With SQL Bot, you can effortlessly generate SQL queries, understand their outputs, and grasp fundamental concepts of SQL programming. Let's dive into the world of SQL together! 💡🔍
+![Screenshot (1334)](https://github.com/aniketandhale08/GenQuery/assets/99685171/4ab80f48-0d36-4224-8f4d-9ee46505f139)
+
+# How to Use SQL Bot
+### Requesting a Query:
+
+Simply tell SQL Bot what records you need from your database by typing your query request.
+For example, you could say "Retrieve all customers from the 'customers' table".
+### Generating SQL Query:
+
+SQL Bot will swiftly generate the SQL query based on your request.
+It will provide you with the code to execute to fetch the desired records.
+### Understanding the Output:
+
+SQL Bot goes the extra mile by explaining the generated SQL query.
+You'll gain insights into how the query works and why it's structured the way it is.
+### Enhancing SQL Skills:
+
+This tool is designed not just for data retrieval but also for learning SQL programming.
+Each query generated comes with detailed explanations to help you grasp SQL concepts effectively.
+
+### Formatting SQL Queries:
+Use the SQL Formatter to clean up and organize your SQL code. 
+This feature ensures that your queries are readable and consistent, making them easier to understand and maintain.
+
+### Explaining SQL Queries:
+Use the Query Explainer to break down complex SQL queries into understandable parts. 
+This feature provides detailed explanations of each component of the query, helping you understand how and why it works.
+
+### Why Use SQL Bot?
+**Streamline Querying:** No need to write complex SQL queries from scratch. Let SQL Bot do the heavy lifting for you.
+
+**Learn by Doing:** Interact with SQL Bot to understand SQL syntax, query structure, and best practices.
+
+**Save Time:** Instantly generate SQL queries without spending hours figuring out the right syntax.
+
+**Beginner-Friendly:** Perfect for beginners looking to dive into SQL programming with ease.
+
---
+++ app.py
@@ -0,0 +1,176 @@
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel("gemini-pro")
+    return model
+
+def generate_sql_query(model, input_prompt):
+    template = """
+        Create a SQL query snippet using the below text:
+
+        ```
+        {text_input}
+        ```  
+        i just want a Sql query         
+    """
+    formatted_template = template.format(text_input=input_prompt)
+    response = model.generate_content(formatted_template)
+    sql_query = response.text.strip().lstrip("```sql").rstrip("```")
+    return sql_query
+
+def generate_expected_output(model, sql_query):
+    expected_output = """
+        What would be the expected response of this SQL query snippet:
+
+        ```
+        {sql_query}
+        ```  
+        Provide sample table response with no explanation        
+    """
+    expected_output_formatted = expected_output.format(sql_query=sql_query)
+    response = model.generate_content(expected_output_formatted)
+    return response.text
+
+def generate_explanation(model, sql_query):
+    explanation = """
+        Explain this SQL query:
+
+        ```
+        {sql_query}
+        ```  
+        Please provide the simplest explanation:       
+    """
+    explanation_formatted = explanation.format(sql_query=sql_query)
+    response = model.generate_content(explanation_formatted)
+    return response.text
+
+def sql_formatter(model, sql_code):
+    template = """
+        Format this SQL code block:
+
+        ```
+        {sql_code}
+        ```  
+        Format this SQL code        
+    """
+    formatted_template = template.format(sql_code=sql_code)
+    response = model.generate_content(formatted_template)
+    formatted_sql = response.text.strip().lstrip("```sql").rstrip("```")
+    return formatted_sql
+
+def query_explainer(model, sql_syntax):
+    explanation = """
+        Explain each part of this SQL query:
+
+        ```
+        {sql_syntax}
+        ```  
+        Please break down the query and explain each important concept or word:      
+    """
+    explanation_formatted = explanation.format(sql_syntax=sql_syntax)
+    response = model.generate_content(explanation_formatted)
+    return response.text
+
+def main():
+    model = configure()
+    st.set_page_config(page_title="GenQuery", page_icon="robot:")
+
+    st.sidebar.title('Navigation')
+    pages = st.sidebar.radio("Go to", ['About', 'SQL Query Generator', 'SQL Formatter', 'Query Explainer'])
+
+    if pages == 'About':
+        st.markdown(
+            """
+            <div style="text-align:center;">
+            <h1>GenQuery 🤖</h1>
+            <h3>Your Personal SQL Query Assistant</h3>
+            <p> Welcome to GenQuery! Our project is your personal SQL query assistant powered by Google's Generative AI tools. 
+            With GenQuery, you can effortlessly generate SQL queries and receive detailed explanations, and also format your for readability and consistency. Let's simplifying your data retrieval process!</p>           
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+    elif pages == 'SQL Query Generator':
+        st.markdown(
+            """
+            <div style="text-align:center;">
+            <h1>SQL Query Generator 📝</h1>
+            <p>Use the SQL Query Generator to create SQL queries from natural language prompts.</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+        text_input = st.text_area("Type your desired query below to unlock the power of SQL Query Genie! 💬")
+        submit = st.button("Generate SQL Query")
+
+        if submit:
+            with st.spinner("Generating SQL Query.."):
+                sql_query = generate_sql_query(model, text_input)
+                eoutput = generate_expected_output(model, sql_query)
+                explanation = generate_explanation(model, sql_query)
+                with st.container():
+                    st.success("Your SQL query has been successfully generated. Feel free to copy and paste it into your database management system to retrieve the requested records.")
+                    st.code(sql_query, language="sql")
+
+                    st.markdown(
+                        """
+                        <div style="background-color: #d4edda; padding: 10px; border-radius: 5px;">
+                            Expected output of this SQL Query.<br>
+                            If the structure of the query isn't displayed, please click again on the 'Generate SQL Query' button.
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )
+                    st.markdown(eoutput)
+
+                    st.success("Explanation of SQL Query")
+                    st.markdown(explanation)
+
+    elif pages == 'SQL Formatter':
+        st.markdown(
+            """
+            <div style="text-align:center;">
+            <h1>SQL Formatter 📋</h1>
+            <p>Use the SQL formatter to format your SQL queries for readability and consistency.</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+        sql_input = st.text_area("Paste your SQL code here:")
+        format_button = st.button("Format SQL")
+
+        if format_button:
+            if sql_input:
+                formatted_sql = sql_formatter(model, sql_input)
+                st.code(formatted_sql, language='sql')
+
+    elif pages == 'Query Explainer':
+        st.markdown(
+            """
+            <div style="text-align:center;">
+            <h1>Query Explainer 📢</h1>
+            <p>Understand each part of your SQL query with explanations.</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+        sql_syntax = st.text_area("Paste your SQL syntax here:")
+        explain_button = st.button("Explain Query")
+
+        if explain_button:
+            if sql_syntax:
+                explanation = query_explainer(model, sql_syntax)
+                st.markdown(explanation)
+
+if __name__ == "__main__":
+    main()
